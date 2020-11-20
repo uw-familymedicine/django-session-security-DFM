d2acd171f7a1c9d95d959682ef1bf9b4812d5227
jstevensmedex <41127696+jstevensmedex@users.noreply.github.com>
2020-11-19 17:59:33 -0800

    add code for aa redirect

--- session_security/middleware.py
+++ session_security/middleware.py
@@ -86,7 +86,7 @@ class SessionSecurityMiddleware(MiddlewareMixin):
                 return HttpResponseRedirect('/loggedout/')
             else:
                 #return HttpResponseRedirect(reverse('shibboleth:logout'))
-                return HttpResponseRedirect('/shib/logout/')
+                return HttpResponseRedirect('/lougout/')
         elif (request.path == reverse('session_security_ping') and
                 'idleFor' in request.GET):
             self.update_last_activity(request, now)
